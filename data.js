46a3013956a6baf65bb34099013daafa713deaf6
SectorChan1234 <worksectorchan@gmail.com>
2026-05-10 22:35:41 +0800

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@
 const rawItems = [
               
                 {   
-                    id: 10, 
+                    id: 1, 
                     type: 'video', 
                     title: '10沙田複式戶', 
                     tags: ['豪宅', '特色戶'], 
@@ -10,7 +10,147 @@ const rawItems = [
                     embedCode: '<iframe src="https://www.youtube.com/embed/cFKQCfP5sZA"></iframe>' 
                 }, 
                 { 
+                    id: 2, 
+                    type: 'images', 
+                    title: '【全新】九龍塘三房實拍', 
+                    tags: ['九龍塘', '三房', '全新裝修'], 
+                    images: 
+                    [
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+  
+                    ],
+                    videoUrl: '' // 圖片模式可留空
+                },
+                  { 
+                    id: 3, 
+                    type: 'images', 
+                    title: '【全新】九龍塘三房實拍', 
+                    tags: ['九龍塘', '三房', '全新裝修'], 
+                    images: 
+                    [
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+  
+                    ],
+                    videoUrl: '' // 圖片模式可留空
+                },
+                  { 
+                    id: 4, 
+                    type: 'images', 
+                    title: '【全新】九龍塘三房實拍', 
+                    tags: ['九龍塘', '三房', '全新裝修'], 
+                    images: 
+                    [
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+  
+                    ],
+                    videoUrl: '' // 圖片模式可留空
+                },
+                  { 
+                    id: 5, 
+                    type: 'images', 
+                    title: '【全新】九龍塘三房實拍', 
+                    tags: ['九龍塘', '三房', '全新裝修'], 
+                    images: 
+                    [
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+  
+                    ],
+                    videoUrl: '' // 圖片模式可留空
+                },
+                  { 
+                    id: 6, 
+                    type: 'images', 
+                    title: '【全新】九龍塘三房實拍', 
+                    tags: ['九龍塘', '三房', '全新裝修'], 
+                    images: 
+                    [
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+  
+                    ],
+                    videoUrl: '' // 圖片模式可留空
+                },
+                  { 
+                    id: 7, 
+                    type: 'images', 
+                    title: '【全新】九龍塘三房實拍', 
+                    tags: ['九龍塘', '三房', '全新裝修'], 
+                    images: 
+                    [
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+  
+                    ],
+                    videoUrl: '' // 圖片模式可留空
+                },
+                  { 
+                    id: 8, 
+                    type: 'images', 
+                    title: '【全新】九龍塘三房實拍', 
+                    tags: ['九龍塘', '三房', '全新裝修'], 
+                    images: 
+                    [
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+  
+                    ],
+                    videoUrl: '' // 圖片模式可留空
+                },
+                  { 
+                    id: 9, 
+                    type: 'images', 
+                    title: '【全新】九龍塘三房實拍', 
+                    tags: ['九龍塘', '三房', '全新裝修'], 
+                    images: 
+                    [
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+  
+                    ],
+                    videoUrl: '' // 圖片模式可留空
+                },
+                  { 
+                    id: 10, 
+                    type: 'images', 
+                    title: '【全新】九龍塘三房實拍', 
+                    tags: ['九龍塘', '三房', '全新裝修'], 
+                    images: 
+                    [
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+  
+                    ],
+                    videoUrl: '' // 圖片模式可留空
+                },
+                  { 
                     id: 11, 
+                    type: 'images', 
+                    title: '【全新】九龍塘三房實拍', 
+                    tags: ['九龍塘', '三房', '全新裝修'], 
+                    images: 
+                    [
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+  
+                    ],
+                    videoUrl: '' // 圖片模式可留空
+                },
+                  { 
+                    id: 12, 
                     type: 'images', 
                     title: '【全新】九龍塘三房實拍', 
                     tags: ['九龍塘', '三房', '全新裝修'], 
