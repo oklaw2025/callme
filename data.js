879e7d06aeaa97210197a946ca4f4baff226c16b
SectorChan1234 <worksectorchan@gmail.com>
2026-05-10 20:19:51 +0800

    Add rawItems array with property data

---
+++ data.js
@@ -0,0 +1,30 @@
+// data.js
+const rawItems = [
+    { 
+        id: 10, 
+        type: 'images', 
+        title: '九龍站頂級豪宅 - 相片集', 
+        tags: ['豪宅', '九龍', '三房'], 
+        images: [
+            'https://picsum.photos/id/101/1200/800',
+            'https://picsum.photos/id/102/1200/800',
+            'https://picsum.photos/id/103/1200/800'
+        ]
+    },
+    { 
+        id: 9, 
+        type: 'video', 
+        title: '最新：西九龍四房豪宅', 
+        tags: ['豪宅', '西九龍', '四房'], 
+        videoUrl: 'https://youtu.be/gPoaUt3Fnpk', 
+        embedCode: '<iframe src="https://www.youtube.com/embed/gPoaUt3Fnpk"></iframe>' 
+    },
+    { 
+        id: 8, 
+        type: 'video', 
+        title: '屯門兩房 - 罕有放盤', 
+        tags: ['上車盤', '屯門', '兩房'], 
+        videoUrl: 'https://youtu.be/dQw4w9WgXcQ', 
+        embedCode: '<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>' 
+    }
+];
