f5f51bfc923852d8ebf607d77729ad8d767382df
SectorChan1234 <worksectorchan@gmail.com>
2026-05-10 20:21:07 +0800

    Add new video and image entries to rawItems

--- data.js
+++ data.js
@@ -1,30 +1,44 @@
 // data.js
 const rawItems = [
-    { 
-        id: 10, 
-        type: 'images', 
-        title: '九龍站頂級豪宅 - 相片集', 
-        tags: ['豪宅', '九龍', '三房'], 
-        images: [
-            'https://picsum.photos/id/101/1200/800',
-            'https://picsum.photos/id/102/1200/800',
-            'https://picsum.photos/id/103/1200/800'
-        ]
-    },
-    { 
-        id: 9, 
-        type: 'video', 
-        title: '最新：西九龍四房豪宅', 
-        tags: ['豪宅', '西九龍', '四房'], 
-        videoUrl: 'https://youtu.be/gPoaUt3Fnpk', 
-        embedCode: '<iframe src="https://www.youtube.com/embed/gPoaUt3Fnpk"></iframe>' 
-    },
-    { 
-        id: 8, 
-        type: 'video', 
-        title: '屯門兩房 - 罕有放盤', 
-        tags: ['上車盤', '屯門', '兩房'], 
-        videoUrl: 'https://youtu.be/dQw4w9WgXcQ', 
-        embedCode: '<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>' 
-    }
+              { id: 1, type: 'video', title: '最新：西九龍四房豪宅', tags: ['豪宅', '西九龍', '四房'], videoUrl: 'https://youtu.be/gPoaUt3Fnpk', embedCode: '<iframe src="https://www.youtube.com/embed/gPoaUt3Fnpk"></iframe>' },
+
+                { id: 2, type: 'video', title: '屯門兩房 - 罕有放盤', tags: ['上車盤', '屯門', '兩房'], videoUrl: 'https://youtu.be/dQw4w9WgXcQ', embedCode: '<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>' },
+
+                { id: 3, type: 'video', title: '啟德新區 - 投資必選', tags: ['投資', '啟德'], videoUrl: 'https://youtu.be/dQw4w9WgXcQ', embedCode: '<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>' },
+
+                { id: 4, type: 'video', title: '元朗大屋苑 - 園景三房', tags: ['元朗', '三房'], videoUrl: 'https://youtu.be/dQw4w9WgXcQ', embedCode: '<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>' },
+
+                { id: 5, type: 'video', title: '將軍澳地鐵沿線', tags: ['將軍澳', '兩房'], videoUrl: 'https://youtu.be/dQw4w9WgXcQ', embedCode: '<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>' },
+
+                { id: 6, type: 'video', title: '大埔村屋高層', tags: ['村屋', '大埔'], videoUrl: 'https://youtu.be/dQw4w9WgXcQ', embedCode: '<iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ"></iframe>' },
+
+                { id: 7, type: 'video', title: '沙田複式戶', tags: ['豪宅', '特色戶'], videoUrl: 'https://youtu.be/cFKQCfP5sZA', embedCode: '<iframe src="https://www.youtube.com/embed/cFKQCfP5sZA"></iframe>' }, 
+                 
+                { id: 8, type: 'video', title: '沙田複式戶', tags: ['123', 'abc'], videoUrl: 'https://youtu.be/kl4u4LfyvGI', embedCode: '<iframe src="https://www.youtube.com/embed/kl4u4LfyvGI"></iframe>' },
+                { 
+                    id: 9, 
+                    type: 'images', 
+                    title: '【全新】九龍塘三房實拍', 
+                    tags: ['九龍塘', '三房', '全新裝修'], 
+                    images: [
+                        'https://i.lih.kg/thumbnail?u=https%3A%2F%2Fna.cx%2Fi%2F2AgCcqh.jpg&h=b27b556d&s=540',
+                        'https://i.lih.kg/thumbnail?u=https%3A%2F%2Fna.cx%2Fi%2FjejwhpE.webp&h=06edace5&s=540',
+                        'https://i.lih.kg/thumbnail?u=https%3A%2F%2Fna.cx%2Fi%2Fyq2U0mp.jpg&h=ee91f17a&s=540'
+                    ],
+                    videoUrl: '' // 圖片模式可留空
+                },
+                { id: 10, type: 'video', title: '10沙田複式戶', tags: ['豪宅', '特色戶'], videoUrl: 'https://youtu.be/cFKQCfP5sZA', embedCode: '<iframe src="https://www.youtube.com/embed/cFKQCfP5sZA"></iframe>' }, 
+                { 
+                    id: 11, 
+                    type: 'images', 
+                    title: '【全新】九龍塘三房實拍', 
+                    tags: ['九龍塘', '三房', '全新裝修'], 
+                    images: [
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+                        'https://oklaw2025.github.io/callme/photo1.jpg',
+  
+                    ],
+                    videoUrl: '' // 圖片模式可留空
+                },
 ];
