13f231b80182d642f8b1ef8d59625a9dbb1b0d8c
SectorChan1234 <worksectorchan@gmail.com>
2026-05-14 22:31:28 +0800

    Enhance rawItems with price and area attributes
    Added price and area properties to each item.

--- data.js
+++ data.js
@@ -1,30 +1,33 @@
 // data.js
 const rawItems = [
-                { 
-                    id: 14, 
-                    type: 'images', 
-                    title: '迎豐', 
-                    tags: ['宋王臺站', '1房高層', '271呎', '520萬', '樓盤編號:002451', '日期:14/5/2026'], 
-                    baseFolder: 'id14',
-                    numPhotos: 7,      // 總共7張：photo1.jpg + photo2.jpeg ~ photo7.jpeg
-                    videoUrl: ''
-                },
-               {   
-                    id: 13, 
-                    type: 'images', 
-                    title: '城軒', 
-                    tags: ['美善同道', '2房梗廚高層', '510呎', '1100萬', '樓盤編號:002551', '日期:14/5/2026'], 
-                    baseFolder: 'id13',
-                    numPhotos: 7,      // 總共7張：photo1.jpg + photo2.jpeg ~ photo7.jpeg
-                    videoUrl: ''
-                },
-{   
-                    id: 12, 
-                    type: 'images', 
-                    title: '喜築', 
-                    tags: [ '1房低層', '349呎', '550萬', '樓盤編號:002571', '日期:14/5/2026'], 
-                    baseFolder: 'id12',
-                    numPhotos: 5,      // 總共5張：photo1.jpg + photo2.jpeg ~ photo7.jpeg
-                    videoUrl: ''
-                },
+    { 
+        id: 14, 
+        type: 'images', 
+        title: '迎豐', 
+        tags: ['宋王臺站', '1房高層', '271呎', '520萬', '樓盤編號:002451', '日期:14/5/2026'], 
+        baseFolder: 'id14',
+        numPhotos: 7,
+        price: 520,      // 萬
+        area: 271        // 呎
+    },
+    {   
+        id: 13, 
+        type: 'images', 
+        title: '城軒', 
+        tags: ['美善同道', '2房梗廚高層', '510呎', '1100萬', '樓盤編號:002551', '日期:14/5/2026'], 
+        baseFolder: 'id13',
+        numPhotos: 7,
+        price: 1100,
+        area: 510
+    },
+    {   
+        id: 12, 
+        type: 'images', 
+        title: '喜築', 
+        tags: ['1房低層', '349呎', '550萬', '樓盤編號:002571', '日期:14/5/2026'], 
+        baseFolder: 'id12',
+        numPhotos: 5,
+        price: 550,
+        area: 349
+    },
 ];
